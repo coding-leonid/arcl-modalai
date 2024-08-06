39f937a1398de59db136ec15f3c2c1bd06e202e6
voxl2 <root@m0054.com>
2024-08-06 00:47:11 +0000

    Tried to un mavros locally

--- src/robot_bringup/launch/ros_env.bash
+++ src/robot_bringup/launch/ros_env.bash
@@ -17,7 +17,7 @@ unset ROS_HOSTNAME
 # configure ROS IPs here
 export ROS_MASTER_IP=192.168.50.147
 export ROS_IP=192.168.50.16
-export ROS_MASTER_URI=http://${ROS_MASTER_IP}:11311/
-export VICON_IP=192.168.50.26:801
+export ROS_MASTER_URI=http://${ROS_MASTER_IP}:11311
+export VICON_IP=131.215.96.158:801
 # mavros needs to know what PX4's system id is
 export PX4_SYS_ID=1
